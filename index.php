252b0cbe03a0cb3f060a914c7a6b6d70483a82bb
kkpanuwat <panuwat_ys@kkumail.com>
2021-08-20 22:35:25 +0700

    new version

--- index.php
+++ index.php
@@ -15,18 +15,52 @@
             <form action="" method="post">
                 <div class="form">
                     <div class="form-left">
-                        <div class="studentName">
+                        <div class="studentName inputBlog">
                             <label for="fname">รหัสนักศึกษา : </label>
                             <br>
                             <input type="text" name="stdId" class="stdId inputText" placeholder="640000000-1">
                         </div>
-                        <div class="firstName">
-                            <label for="fname">รหัสนักศึกษา : </label>
+                        <div class="firstName inputBlog">
+                            <label for="fname">ชื่อ-สกุล : </label>
                             <br>
-                            <input type="text" name="fname" class="fname inputText" placeholder="640000000-1">
+                            <input type="text" name="fname" class="fname inputText" placeholder="สมชาย รักเรียน">
+                        </div>
+                        <div class="major-grade">
+                            <div class="major">
+                                <label for="majorName">สาขาวิชา : </label>
+                                <br>
+                                <input value="วิทยาการคอมพิวเตอร์ (CS)" list="major" class="major inputText">
+                                <datalist id="major">
+                                    <option value="วิทยาการคอมพิวเตอร์ (CS)">
+                                    <option value="เทคโนโลยีสารสนเทศ (IT)">
+                                    <option value="ภูมิสารสนเทศศาสตร์(GIS)">
+                                </datalist>
+                            </div>
+                            <div class="grade">
+                                <label for="gradeName">ระดับการศึกษา : </label>
+                                <br>
+                                <select name="grade" class="grade inputText">
+                                    <optgroup label="นักศึกษา">
+                                        <option value="1">ระดับชั้นปริญญาตรี</option>
+                                        <option value="2">ระดับชั้นปริญญาโท</option>
+                                        <option value="3">ระดับชั้นปริญญาเอก</option>
+                                    </optgroup>
+                                    <optgroup label="บุคลากร">
+                                        <option value="teacher">อาจารย์</option>
+                                        <option value="staff">บุคลากร</option>
+                                    </optgroup>
+
+                                </select>
+                            </div>
                         </div>
                     </div>
-                    <div class="form-right"></div>
+                    <div class="form-right">
+                        <div class="comment">
+                            <label for="commentName">คำอวยพร :</label>
+                            <br>
+                            <textarea name="comment" class="commentInput" rows="13"></textarea>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
