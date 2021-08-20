2f066e885b14fb2376aa79d0eb8dfb3073bae028
kkpanuwat <panuwat_ys@kkumail.com>
2021-08-20 21:25:05 +0700

    innit version

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teacher's Day | Cs KKU</title>
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="form-control">
+            <form action="" method="post">
+                <div class="form">
+                    <div class="form-left"></div>
+                    <div class="form-right"></div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
