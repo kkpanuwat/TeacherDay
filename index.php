302ac41cede147afb6161f03cfd33fb46adf32af
kkpanuwat <panuwat_ys@kkumail.com>
2021-08-21 22:19:20 +0700

    update teacher

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 <body>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#F4D160" fill-opacity="1" d="M0,192L40,192C80,192,160,192,240,202.7C320,213,400,235,480,208C560,181,640,107,720,101.3C800,96,880,160,960,176C1040,192,1120,160,1200,138.7C1280,117,1360,107,1400,101.3L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
-        <image href="http://cad.kku.ac.th/wp-content/uploads/2018/05/Th_color.png" width="50" x="95%" y="15"/>
-        <image href="assets/logocs.png" width="100" x="88%" y="10"/>
+        <image href="http://cad.kku.ac.th/wp-content/uploads/2018/05/Th_color.png" width="50" x="95%" y="15" />
+        <image href="assets/logocs.png" width="100" x="88%" y="10" />
     </svg>
 
     <div class="container">
@@ -61,12 +61,17 @@
                     </div>
                 </div>
                 <div class="form-right">
+                    <label for="teacherName">ถึง : </label>
+                    <br>
+                    <select name="teacher" id="teacher">
+                        <option value="">ผู้ช่วยศาสตราจารย์ สุมณฑา เกษมวิลาศ</option>
+                    </select>
                     <div class="comment">
                         <label for="commentName">คำอวยพร :</label>
                         <br>
-                        <textarea name="comment" id="comment" class="commentInput"></textarea>
-                    </div>    
-                </div>  
+                        <textarea name="comment" id="comment" class="commentInput-teacher"></textarea>
+                    </div>
+                </div>
             </div>
 
             <button class="btn btn-save" onclick="insertComment()">ส่งคำอวยพร</button>
