d1adf3082d4979ec54c885c3b4700c06f7ce50e0
naratthaa <tanarat@kkumail.com>
2021-08-22 19:28:54 +0700

    Update null

--- dashboard/index.php
+++ dashboard/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/imgs/icon.PNG">
     <link rel="stylesheet" href="../assets/css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../assets/js/dashboard.js"></script>
