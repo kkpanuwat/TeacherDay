9c2969bfd221c4d38c34b17bc51b5271f48b0bbe
naratthaa <tanarat@kkumail.com>
2021-08-22 16:06:10 +0700

    Update Dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="../assets/css/index.css">
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
     <script src="../assets/js/dashboard.js"></script>
 </head>
 
@@ -17,9 +17,29 @@
             <button>Search</button>
         </div>
 
-        <div class="table-result">
+        <!-- <div class="table-result">
             <div id="result"></div>
+        </div> -->
+
+        <div class="form-control">
+            <table>
+                <tr>
+                    <th>ลำดับ</th>
+                    <th>ชื่อผู้ส่ง</th>
+                    <th>ชื่อผู้รับ</th>
+                    <th>วันที่ส่ง</th>
+                    <th>ข้อความ</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Narattha</td>
+                    <td>Sumonta</td>
+                    <td>22-8-2564</td>
+                    <td>สุขสันต์วันเกิดนะค้า</td>
+                </tr>         
+            </table>
         </div>
+
     </div>
 </body>
 
