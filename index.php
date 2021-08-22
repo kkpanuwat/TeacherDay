cc1911b7fc37b72e4ca09edd298b33cccc676943
kkpanuwat <panuwat_ys@kkumail.com>
2021-08-22 16:43:20 +0700

    update

--- index.php
+++ index.php
@@ -70,9 +70,9 @@
             </div>
             <label style="text-align: center; display:block; margin:20px" for="commentName">เลือกพานดอกไม้</label>
             <div class="form-pan">
-                <img class="img-pan" id="pan1" onclick="clickpan('pan1')" src="assets/imgs/pan01.png" alt="">
+                <img class="img-pan" id="pan1" onclick="clickpan('pan1')" src="assets/imgs/pan03.png" alt="">
                 <img class="img-pan" id="pan2" onclick="clickpan('pan2')" src="assets/imgs/pan02.png" alt="">
-                <img class="img-pan" id="pan3" onclick="clickpan('pan3')" src="assets/imgs/pan03.png" alt="">
+                <img class="img-pan" id="pan3" onclick="clickpan('pan3')" src="assets/imgs/pan01.png" alt="">
             </div>
             <button class="btn btn-save" onclick="insertComment()">ส่งคำอวยพร</button>
         </div>
