85d9e71b893af4ea310d7724baf382ce08742c3e
naratthaa <tanarat@kkumail.com>
2021-08-22 16:42:34 +0700

    update button

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../assets/css/dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../assets/js/dashboard.js"></script>
 </head>
 
@@ -26,18 +27,31 @@
                 <tr>
                     <th>ลำดับ</th>
                     <th>ชื่อผู้ส่ง</th>
-                    <th>ชื่อผู้รับ</th>
-                    <th>วันที่ส่ง</th>
                     <th>ข้อความ</th>
+                    <th>วันที่ส่ง</th>
+                    <th>ชื่อผู้รับ</th>
+                    <th></th>
                 </tr>
                 <tr>
                     <td>1</td>
                     <td>Narattha</td>
-                    <td>Sumonta</td>
-                    <td>22-8-2564</td>
                     <td>สุขสันต์วันเกิดนะค้า</td>
-                </tr>         
+                    <td>22-8-2564</td>
+                    <td>Sumonta</td>
+                    <td><button class="btn-trash"><i class="fa fa-trash"></i></button></td>
+                </tr>
             </table>
+            <div class="pager">
+                <ul>
+                    <li>Page <input class="input-page" type="text"> of 500</li>
+                    <li>
+                        <button class="button">Previous</button>
+                    </li>
+                    <li>
+                        <button class="button">Next</button>
+                    </li>
+                </ul>
+            </div>
         </div>
 
     </div>
